b74adf7aefc5a887ca7dac5ebbee09223719618e
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-11-09 20:26:04 +0100

    fixed html in email sent by form

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ if (filter_has_var(INPUT_POST, "submit")){
       $subject = "Contact Form";
       $body = "<h2>Contact form submitted</h2>
       <h4>Name</h4><p>$name</p>
-      <h4>Email</h4>$email</p>
+      <h4>Email</h4><p>$email</p>
       <h4>Message</h4><p>$message</p>";
 
-      $header ="MIME Version: 1.0"."\r\n";
-      $header .= "Content-type:text/html;charset=UTF-8"."\r\n";
-      $header .= "From: ".$name." <".$email.">";
+      $header = "MIME-Version: 1.0"."\r\n";
+      $header .= "Content-Type:text/html;charset=UTF-8"."\r\n";
+      $header .= "From: ".$name."<".$email.">"."\r\n";
       
       if (mail($toEmail, $subject, $body, $header)){
         $msg = "Votre email a été enovyé sans problème!";
